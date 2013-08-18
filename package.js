a943bea4cffeac9a14c2825dfe57d32b9d677e6f
Tom Coleman <tom@thesnail.org>
2013-08-18 15:46:34 +1000

    Support 0.6.5 properly

--- package.js
+++ package.js
@@ -4,5 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('crypto-base', ['client', 'server']);
+  api.imply('crypto-base', ['client', 'server']);
+  
   api.add_files('md5.js', ['client', 'server']);
 });
